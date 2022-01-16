e62bee766303d83d8c986d22aa4a51d52452ec34
min-jo <guny524@gmail.com>
2022-01-16 19:55:25 +0900

    fix(백준1257): backtracking이나 branch and bound로 해야 함
    dp인 경우 가격의 폭이 너무 넓어 느리고,
    지금도 숫자가 큰 경우에 1원인 경우도 큐에 밀어넣어서 느리다.
    또한 10 {1, 4, 5, 7}인 경우 bound가 4로 계산돼서
    coin=5인 경우를 큐에 안 넣는 것 같음
    
    see also: c298ab95f91432fab923350943f431fff7661753

--- baeckjoon/1257/main.go
+++ baeckjoon/1257/main.go
@@ -3,38 +3,85 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
-	"sort"
 	"strconv"
-	"strings"
 )
 
-func Solve(money uint64, coins []int) (cnt uint64) {
-	sort.Sort(sort.Reverse(sort.IntSlice(coins)))
+type Node struct {
+	price uint64 // 현재 까지 선택한 동전 조합의 총 금액
+	cnt   uint64 // cnt == 트리의 depth
+	bound uint64 // 최소 cnt의 한계치, 항상 minCnt <= bound
+}
+
+func GetMaxCoin(coins []uint64) (max uint64) {
 	for _, coin := range coins {
-		div, mod := money/uint64(coin), money%uint64(coin)
-		cnt += div
-		if mod != 0 {
-			money = mod
-		} else {
-			break
+		if max < coin {
+			max = coin
 		}
 	}
 	return
 }
 
+// Solve 멈추는 조건: 금액이 money를 넘어가거나, 현재까지 최소 갯수보다 바운드가 크면 멈춤
+func Solve(money uint64, coins []uint64) uint64 {
+	maxCoin := GetMaxCoin(coins)
+	var minCnt uint64 = math.MaxUint64
+	var minBound uint64 = math.MaxUint64
+	// getBound 전체 money에서 현재 선택한 조합으로 만들어진 price를 뺀 남은 금액을
+	// 가장 큰 동전과 1의 조합으로 메꿨을 때 cnt 값 계산
+	// 이 조합 보다 동전 갯수가 많으면 답이 없다 이런 뜻
+	getBound := func(price, cnt uint64) (bool, uint64) {
+		bound := cnt + (money-price)/maxCoin + (money-price)%maxCoin
+		if bound > minBound {
+			return true, uint64(0) // 가지치기
+		} else {
+			minBound = bound
+			return false, bound
+		}
+	}
+	getBound(0, 0)
+	q := []Node{}
+	q = append(q, Node{0, 0, 0})
+	for len(q) != 0 {
+		// fmt.Println(len(q))
+		n := q[0]
+		if n.bound > minBound {
+			// 큐에 넣을 땐 minBound 보다 작을 수 있지만
+			// 다른 곳에서 minBound가 업데이트 될 수 있으니, 큐에서 꺼낼 때도 검사
+			q = q[1:]
+			continue
+		}
+		for _, coin := range coins { // 자식 노드들 만들기
+			if price := n.price + coin; price > money { // 원하는 금액을 넘어서면
+				continue
+			} else if cnt := n.cnt + 1; price == money && cnt < minCnt { // 원하는 금액이 되면 cnt 업데이트
+				minCnt = cnt
+				continue
+			} else if prune, bound := getBound(price, cnt); prune || bound > minCnt {
+				continue
+			} else {
+				q = append(q, Node{price, cnt, bound})
+			}
+		}
+		q = q[1:]
+	}
+	return minCnt
+}
+
 func main() {
-	var moneyStr, cntStr, coinLineStr string
+	var moneyStr, cntStr, coinStr string
 	reader := bufio.NewReader(os.Stdin)
 	writer := bufio.NewWriter(os.Stdout)
 	defer writer.Flush()
-	fmt.Fscan(reader, &moneyStr, &cntStr, &coinLineStr)
+	fmt.Fscan(reader, &moneyStr, &cntStr)
 	money, _ := strconv.ParseUint(moneyStr, 10, 64)
-	coinsStr := strings.Fields(coinLineStr)
-	coins := []int{}
-	for _, coinStr := range coinsStr {
-		coin, _ := strconv.Atoi(coinStr)
+	cnt, _ := strconv.Atoi(cntStr)
+	coins := []uint64{}
+	for i := 0; i < cnt; i++ {
+		fmt.Fscan(reader, &coinStr)
+		coin, _ := strconv.ParseUint(coinStr, 10, 64)
 		coins = append(coins, coin)
 	}
-	fmt.Fprintln(writer, Solve(money, coins))
+	fmt.Fprint(writer, Solve(money, coins))
 }
--- baeckjoon/1257/main_test.go
+++ baeckjoon/1257/main_test.go
@@ -7,13 +7,13 @@ import (
 
 func TestSolve(t *testing.T) {
 	assert := assert.New(t)
-	assert.Equal(uint64(100001), Solve(1000000001, []int{10000, 1}), "basic")
-	assert.Equal(uint64(1), Solve(3, []int{3, 2, 1}), "test1")
-	assert.Equal(uint64(2), Solve(4, []int{3, 2, 1}), "test2")
-	assert.Equal(uint64(2), Solve(10, []int{1, 4, 5, 7}), "test3")
-	assert.Equal(uint64(8), Solve(107, []int{1, 69, 100}), "test4")
-	assert.Equal(uint64(1000000000000000000), Solve(1000000000000000000, []int{1}), "test5")
-	assert.Equal(uint64(10), Solve(100000, []int{1, 10000, 10000}), "test6")
-	assert.Equal(uint64(2), Solve(8, []int{1, 4, 5, 6}), "test7")
-	assert.Equal(uint64(3), Solve(65, []int{1, 4, 30, 31}), "test8")
+	// assert.Equal(uint64(100001), Solve(1000000001, []uint64{10000, 1}), "basic")
+	assert.Equal(uint64(1), Solve(3, []uint64{3, 2, 1}), "test1")
+	assert.Equal(uint64(2), Solve(4, []uint64{3, 2, 1}), "test2")
+	assert.Equal(uint64(2), Solve(10, []uint64{1, 4, 5, 7}), "test3")
+	assert.Equal(uint64(8), Solve(107, []uint64{1, 69, 100}), "test4")
+	// assert.Equal(uint64(1000000000000000000), Solve(1000000000000000000, []uint64{1}), "test5")
+	assert.Equal(uint64(10), Solve(100000, []uint64{1, 10000, 10000}), "test6")
+	assert.Equal(uint64(2), Solve(8, []uint64{1, 4, 5, 6}), "test7")
+	assert.Equal(uint64(3), Solve(65, []uint64{1, 4, 30, 31}), "test8")
 }
